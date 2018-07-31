c82de9fcb1c0bb832ac514556396b5ee2cb8455c
elizabeth1018 <lizz_101@icloud.com>
2018-07-31 20:05:31 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,18 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
+    @name="Elizabeth"
+    @num= 4
+    @number= 7
     erb :index
   end
 
+  get '/tacos' do
+    erb :tacos
+  end
+  
+  get '/pizza' do
+    erb :pizza
+  end
+  
 end
